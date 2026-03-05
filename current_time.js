5bd58f6c1fb1f55f9820513032e3483799ed8dc8
DiegoAraujoSXT <30546383+DiegoAraujoSXT@users.noreply.github.com>
2026-03-05 15:41:33 -0300

    First commit

---
+++ current_time.js
@@ -0,0 +1,14 @@
+export default class HoraAtual {
+
+    static obterHoraAtual() {
+
+        const agora = new Date()
+
+        const horas = agora.getHours().toString().padStart(2, '0')
+        const minutos = agora.getMinutes().toString().padStart(2, '0')
+        const segundos = agora.getSeconds().toString().padStart(2, '0')
+
+        return `${horas}:${minutos}:${segundos}`
+    }
+
+}
